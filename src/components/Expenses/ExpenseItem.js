480639d3fd8ef19f13f28804238455bd6e9478b3
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-08-17 18:40:57 +0300

    Adds state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
+import React, {useState} from "react";
+
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    //State
+    const [title, setTitle] = useState(props.expenseTitle);
+    
+    //Handlers
+    const changeTitleHandler = () => {
+        setTitle('This is the new title');
+        console.log(title);
+    };
+    
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.expenseDate}/>
             <div className="expense-item__description">
-                <h2>{props.expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.expenseAmount}</div>
             </div>
+            <button onClick={changeTitleHandler}>Change Title</button>
         </Card>
     );
 };
